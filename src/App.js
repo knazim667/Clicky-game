8983e90c5c94c06ae2d02117772902c6fddf4f9a
Nazam <nazamzahoor@yahoo.com>
2018-10-11 22:34:12 -0700

    Jumbotron Add complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 //import Wrapper from "./components/Wrapper";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Jumbotron from './components/Jumbotron';
 //import Footer from "./components/Footer";
 //import Header from "./components/Header";
 
@@ -10,6 +11,7 @@ const App = () => (
   <Router>
     <div>
     <Navbar />
+    <Jumbotron />
     </div>
     
   </Router>
