87c2b98ff51080dd013fd585c69fb28ea712b067
Nazam <nazamzahoor@yahoo.com>
2018-10-13 12:38:18 -0700

    build an app

---
+++ build/precache-manifest.0aa007a3cf9691fdd17318032fa4f65a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "98e3255c96ff5660fcc2",
+    "url": "/static/js/main.98e3255c.chunk.js"
+  },
+  {
+    "revision": "ad27191d041ebe4f9041",
+    "url": "/static/js/1.ad27191d.chunk.js"
+  },
+  {
+    "revision": "98e3255c96ff5660fcc2",
+    "url": "/static/css/main.f71837b4.chunk.css"
+  },
+  {
+    "revision": "f4f6e32d181def06103ec05ddc0aef7d",
+    "url": "/index.html"
+  }
+];
