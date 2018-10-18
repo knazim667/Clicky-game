7498dcf7ddca4fb1f69dcf4da7c386b6a47e5860
Nazam <nazamzahoor@yahoo.com>
2018-10-17 18:22:00 -0700

    Updates

---
+++ precache-manifest.8ce4cfd96e415a054e0f6e2d75e6bbf5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "33333d43910eb172a1b0",
+    "url": "/clicky-game/static/js/main.33333d43.chunk.js"
+  },
+  {
+    "revision": "211bd734e599355549f1",
+    "url": "/clicky-game/static/js/1.211bd734.chunk.js"
+  },
+  {
+    "revision": "33333d43910eb172a1b0",
+    "url": "/clicky-game/static/css/main.a92835b4.chunk.css"
+  },
+  {
+    "revision": "33dcf8eebfe3c6477706d0ae939e8177",
+    "url": "/clicky-game/index.html"
+  }
+];
